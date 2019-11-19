371c7debda03bc96477836304a07c0e49d8afa74
Mika Alaoutinen <42463672+mika-alaoutinen@users.noreply.github.com>
2019-11-19 17:08:20 +0200

    Wine (#1)
    * Added v-for to loop through data.
    
    * Added wine form.
    
    * Adding wines works.
    
    * Added radio buttons for wine type.
    
    * Form validation WIP.
    
    * Added validation via HTML for now.
    
    * Delete wines.
    
    * Working on editing wine.
    
    * Editing wines works.

---
+++ src/components/WineForm.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="wine-form">
+    <form @submit.prevent="submitForm">
+      <fieldset id="add-wine">
+        <legend>Lisää uusi viini</legend>
+        <label>Nimi</label>
+        <input type="text" required v-model="wine.name"/>
+        <label for="wine-types">Viinin tyyppi</label>
+        <div class="radiobuttons" id="wine-types">
+          <input type="radio" name="wine-type" id="kuohu" value="SPARKLING" required v-model="wine.type">
+          <label for="kuohu">Kuohuviini</label>
+          <input type="radio" name="wine-type" id="puna" value="RED" v-model="wine.type">
+          <label for="puna">Punaviini</label>
+          <input type="radio" name="wine-type" id="rose" value="ROSE" v-model="wine.type">
+          <label for="rose">Roseviini</label>
+          <input type="radio" name="wine-type" id="valko" value="WHITE" v-model="wine.type">
+          <label for="valko">Valkoviini</label>
+          <input type="radio" name="wine-type" id="muu" value="OTHER" v-model="wine.type">
+          <label for="muu">Muu viini</label>
+        </div>
+        <label>Maa</label>
+        <input v-model="wine.country" type="text" />
+        <label>Hinta (€)</label>
+        <input v-model="wine.price" type="text" />
+        <label>Määrä (l)</label>
+        <input v-model="wine.quantity" type="text" />
+        <label>Kuvaus</label>
+        <input v-model="wine.description" type="text" /> <!-- TODO: change to array -->
+        <label>Sopii nautittavaksi</label>
+        <input v-model="wine.foodPairings" type="text" /> <!-- TODO: change to array -->
+        <label>URL</label>
+        <input v-model="wine.url" type="text" />
+        <p><button>Lisää viini</button></p>
+      </fieldset>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "wine-form",
+  data() {
+    return {
+      wine: {
+        name: "",
+        type: "",
+        country: "",
+        price: "",
+        quantity: "",
+        description: [],
+        foodPairings: [],
+        url: ""
+      }
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$emit("add:wine", this.wine)
+    }
+  }
+};
+</script>
+
+<style scoped>
+  form { margin-bottom: 2rem }
+  input[type=text] {
+    display: inline-block;
+    width: 50%
+  }
+  .radiobuttons label {
+    display: inline;
+    margin-left: 0.5em;
+    margin-right: 1em;
+  }
+</style>
--- src/components/WineTable.vue
+++ src/components/WineTable.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="wine-table">
-    <table>
+    <!-- Show info message if wine table is empty: -->
+    <p v-if="wines.length < 1" class="empty-table">Ei viinejä</p>    
+    <table v-else>
       <thead>
         <tr>
           <th>Nimi</th>
@@ -14,25 +16,26 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Valkoviini 1</td>
-          <td>Valko</td>
-          <td>Espanja</td>
-          <td>9.50</td>
-          <td>0.75</td>
-          <td>Makea ja hedelmäinen</td>
-          <td>Seurustelujuoma, salaatit ja kasvisruoka</td>
-          <td>blank</td>
-        </tr>
-        <tr>
-          <td>Valkoviini 1</td>
-          <td>Valko</td>
-          <td>Espanja</td>
-          <td>9.50</td>
-          <td>0.75</td>
-          <td>Makea ja hedelmäinen</td>
-          <td>Seurustelujuoma, salaatit ja kasvisruoka</td>
-          <td>blank</td>
+        <tr v-for="wine in wines" :key="wine.id">
+          <td v-if="editing === wine.id">
+            <input type="text" v-model="wine.name">
+          </td>
+          <td v-else>{{ wine.name }}</td>
+          <td>{{ wine.type }}</td>
+          <td>{{ wine.country }}</td>
+          <td>{{ wine.price }}</td>
+          <td>{{ wine.quantity }}</td>
+          <td>{{ wine.description }}</td>
+          <td>{{ wine.foodPairings }}</td>
+          <td>{{ wine.url }}</td>
+          <td v-if="editing === wine.id">
+            <button @click="editWine(wine)">Tallenna</button>
+            <button class="muted-button" @click="cancelEdit(wine)">Peruuta</button>
+          </td>
+          <td v-else>
+            <button @click="editMode(wine)">Muokkaa</button>
+            <button @click="$emit('delete:wine', wine.id)">Poista</button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -41,10 +44,38 @@
 
 <script>
   export default {
-    name: "wine-table"
+    name: "wine-table",
+    data() {
+      return {
+        editing: null
+      }
+    },
+    methods: {
+      editMode(wine) {
+        this.cachedWine = Object.assign({}, wine);
+        this.editing = wine.id;
+      },
+      editWine(wine) {
+        const wineHasEmptyValue = Array
+            .from(Object.values(wine))
+            .some(value => value === "" || value === []);
+        if (wineHasEmptyValue) {
+          return;
+        }
+        this.$emit("edit:wine", wine.id, wine);
+        this.editing = null;
+      },
+      cancelEdit(wine) {
+        Object.assign(wine, this.cachedWine);
+        this.editing = null;
+      }
+    },
+    props: {
+      wines: Array,
+    }
   };
 </script>
 
 <style scoped>
-
+  button { margin: 0 0.5rem 0 0 }
 </style>
