453713a90505e5e9bf1f992f8849e1823bb49232
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-11-19 15:36:06 +0200

    Added validation via HTML for now.

--- src/components/WineForm.vue
+++ src/components/WineForm.vue
@@ -4,32 +4,24 @@
       <fieldset id="add-wine">
         <legend>Lisää uusi viini</legend>
         <label>Nimi</label>
-        <input
-          ref="first"
-          type="text"
-          :class="{ 'has-error': submitting && formHasErrors }"
-          v-model="wine.name"
-          @focus="clearStatus"
-          @keypress="clearStatus"/>
-          
+        <input type="text" required v-model="wine.name"/>
         <div class="radiobuttons">
-          <input v-model="wine.type" type="radio" name="wine-type" id="kuohu" value="SPARKLING">
+          <input type="radio" name="wine-type" id="kuohu" value="SPARKLING" required v-model="wine.type">
           <label for="kuohu">Kuohuviini</label>
-          <input v-model="wine.type" type="radio" name="wine-type" id="puna" value="RED">
+          <input type="radio" name="wine-type" id="puna" value="RED" v-model="wine.type">
           <label for="puna">Punaviini</label>
-          <input v-model="wine.type" type="radio" name="wine-type" id="rose" value="ROSE">
+          <input type="radio" name="wine-type" id="rose" value="ROSE" v-model="wine.type">
           <label for="rose">Roseviini</label>
-          <input v-model="wine.type" type="radio" name="wine-type" id="valko" value="WHITE">
+          <input type="radio" name="wine-type" id="valko" value="WHITE" v-model="wine.type">
           <label for="valko">Valkoviini</label>
-          <input v-model="wine.type" type="radio" name="wine-type" id="muu" value="OTHER">
+          <input type="radio" name="wine-type" id="muu" value="OTHER" v-model="wine.type">
           <label for="muu">Muu viini</label>
         </div>
-        
         <label>Maa</label>
         <input v-model="wine.country" type="text" />
         <label>Hinta (€)</label>
         <input v-model="wine.price" type="text" />
-        <label>Määrä</label>
+        <label>Määrä (l)</label>
         <input v-model="wine.quantity" type="text" />
         <label>Kuvaus</label>
         <input v-model="wine.description" type="text" /> <!-- TODO: change to array -->
@@ -37,10 +29,6 @@
         <input v-model="wine.foodPairings" type="text" /> <!-- TODO: change to array -->
         <label>URL</label>
         <input v-model="wine.url" type="text" />
-
-        <p v-if="error && submitting" class="error-message">Täytä viinin nimi!</p>
-        <p v-if="success" class="success-message">Uusi viini lisätty.</p>
-        
         <p><button>Lisää viini</button></p>
       </fieldset>
     </form>
@@ -52,9 +40,6 @@ export default {
   name: "wine-form",
   data() {
     return {
-      submitting: false,
-      error: false,
-      success: false,
       wine: {
         name: "",
         type: "",
@@ -67,47 +52,9 @@ export default {
       }
     }
   },
-  computed: {
-    /* Is true if any text input in form is empty.
-       Otherwise is false, indicating that form is valid. */
-    formHasErrors() {
-      return Array
-          .from(document.querySelectorAll("#wine-form input[type=text]"))
-          .some(input => input.value == "");
-    }
-  },
   methods: {
     submitForm() {
-      this.submitting = true;
-      this.clearStatus();
-
-      if (this.formHasErrors) {
-        this.error = true;
-        return;
-      }
-
       this.$emit("add:wine", this.wine)
-      // TODO: make into function
-      this.wine = {
-        name: "",
-        type: "",
-        country: "",
-        price: "",
-        quantity: "",
-        description: [],
-        foodPairings: [],
-        url: ""
-      }
-      this.error = false;
-      this.success = true;
-      this.success = false;
-    },
-    clearStatus() {
-      this.success = false;
-      this.error = false;
-    },
-    validateForm() {
-      // validate that 1) text fields are not empty and 2) one radio buttons is selected.
     }
   }
 };
@@ -119,9 +66,6 @@ export default {
     display: inline-block;
     width: 50%
   }
-  [class*="-message"] { font-weight: 500 }
-  .error-message { color: #d33c40 }
-  .success-message { color: #32a95d }
   .radiobuttons label {
     display: inline;
     margin-left: 0.5em;
