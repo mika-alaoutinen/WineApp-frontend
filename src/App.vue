d750443592adde813c60ad08bac3cbbe5e4f004d
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-11-19 11:23:39 +0200

    Added WineTable component with hard coded data.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Viinit</h1>
+    <wine-table/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WineTable from './components/WineTable.vue'
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    WineTable
   }
 }
 </script>
---
+++ src/components/WineTable.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="wine-table">
+    <table>
+      <thead>
+        <tr>
+          <th>Nimi</th>
+          <th>Tyyppi</th>
+          <th>Maa</th>
+          <th>Hinta (€)</th>
+          <th>Määrä (l)</th>
+          <th>Kuvaus</th>
+          <th>Ruokasuositukset</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Valkoviini 1</td>
+          <td>Valko</td>
+          <td>Espanja</td>
+          <td>9.50</td>
+          <td>0.75</td>
+          <td>Makea ja hedelmäinen</td>
+          <td>Seurustelujuoma, salaatit ja kasvisruoka</td>
+          <td>blank</td>
+        </tr>
+        <tr>
+          <td>Valkoviini 1</td>
+          <td>Valko</td>
+          <td>Espanja</td>
+          <td>9.50</td>
+          <td>0.75</td>
+          <td>Makea ja hedelmäinen</td>
+          <td>Seurustelujuoma, salaatit ja kasvisruoka</td>
+          <td>blank</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "wine-table"
+  };
+</script>
+
+<style scoped>
+
+</style>
