f22ef7650369d80e000a967ee9e24b41c5704f3e
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-11-19 13:46:45 +0200

    Adding wines works.

--- src/components/WineForm.vue
+++ src/components/WineForm.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="wine-form">
-    <form>
+    <form @submit.prevent="submitForm">
       <fieldset id="add-wine">
         <legend>Add new wine</legend>
         <label>Name</label>
-        <input type="text" />
+        <input v-model="wine.name" type="text" />
         <label>Type</label>
-        <input type="text" />
+        <input v-model="wine.type" type="text" />
         <label>Country</label>
-        <input type="text" />
+        <input v-model="wine.country" type="text" />
         <label>Price</label>
-        <input type="text" />
+        <input v-model="wine.price" type="text" />
         <label>Quantity</label>
-        <input type="text" />
+        <input v-model="wine.quantity" type="text" />
         <label>Description</label>
-        <input type="text" /> <!-- TODO: change to array -->
+        <input v-model="wine.description" type="text" /> <!-- TODO: change to array -->
         <label>Food pairings</label>
-        <input type="text" /> <!-- TODO: change to array -->
+        <input v-model="wine.foodPairings" type="text" /> <!-- TODO: change to array -->
         <label>URL</label>
-        <input type="text" />
+        <input v-model="wine.url" type="text" />
         <p><button>Add Wine</button></p>
       </fieldset>
     </form>
@@ -40,6 +40,11 @@ export default {
         foodPairings: [],
         url: ""
       }
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$emit("add:wine", this.wine)
     }
   }
 };
