290bae2e3518826f9aa35e91af8b9926a5c74e20
Mika Alaoutinen <42463672+mika-alaoutinen@users.noreply.github.com>
2019-11-22 19:18:31 +0200

    Development (#3)
    * Changed index.html title and added Primitive UI style.
    
    * Added WineTable component with hard coded data.
    
    * Wine (#1)
    
    * Added v-for to loop through data.
    
    * Added wine form.
    
    * Adding wines works.
    
    * Added radio buttons for wine type.
    
    * Form validation WIP.
    
    * Added validation via HTML for now.
    
    * Delete wines.
    
    * Working on editing wine.
    
    * Editing wines works.
    
    * Routing (#2)
    
    * Added axios dependency.
    
    * WIP get wines.
    
    * Removed HelloWorld.vue.
    
    * Minor refactoring.
    
    * Working on CRUD backend calls.
    
    * Added WineService.js

---
+++ src/components/WineTable.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="wine-table">
+    <!-- Show info message if wine table is empty: -->
+    <p v-if="wines.length < 1" class="empty-table">Ei viinejä</p>    
+    <table v-else>
+      <thead>
+        <tr>
+          <th>Nimi</th>
+          <th>Tyyppi</th>
+          <th>Maa</th>
+          <th>Hinta (€)</th>
+          <th>Määrä (l)</th>
+          <th>Kuvaus</th>
+          <th>Ruokasuositukset</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- TODO: figure out a smarter way to edit wines. -->
+        <tr v-for="wine in wines" :key="wine.id">
+          <td v-if="editing === wine.id">
+            <input type="text" v-model="wine.name">
+          </td>
+          <td v-else>{{ wine.name }}</td>
+          <td>{{ wine.type }}</td>
+          <td>{{ wine.country }}</td>
+          <td>{{ wine.price }}</td>
+          <td>{{ wine.quantity }}</td>
+          <td>{{ wine.description }}</td>
+          <td>{{ wine.foodPairings }}</td>
+          <td>{{ wine.url }}</td>
+          <td v-if="editing === wine.id">
+            <button @click="editWine(wine)">Tallenna</button>
+            <button class="muted-button" @click="cancelEdit(wine)">Peruuta</button>
+          </td>
+          <td v-else>
+            <button @click="editMode(wine)">Muokkaa</button>
+            <button @click="$emit('delete:wine', wine.id)">Poista</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "wine-table",
+    data() {
+      return {
+        editing: null
+      }
+    },
+    methods: {
+      editMode(wine) {
+        this.cachedWine = Object.assign({}, wine);
+        this.editing = wine.id;
+      },
+      editWine(wine) {
+        const wineHasEmptyValue = Array
+            .from(Object.values(wine))
+            .some(value => value === "" || value === []);
+        if (wineHasEmptyValue) {
+          return;
+        }
+        this.$emit("edit:wine", wine.id, wine);
+        this.editing = null;
+      },
+      cancelEdit(wine) {
+        Object.assign(wine, this.cachedWine);
+        this.editing = null;
+      }
+    },
+    props: {
+      wines: Array,
+    }
+  };
+</script>
+
+<style scoped>
+  button { margin: 0 0.5rem 0 0 }
+</style>
