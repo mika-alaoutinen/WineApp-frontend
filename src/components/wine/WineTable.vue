631e404a9e2a826db2e4c411da8ba0db1a4e0ab7
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-12-03 19:21:23 +0200

    Trimmed extra fields from wine table.

--- src/components/wine/WineTable.vue
+++ src/components/wine/WineTable.vue
@@ -5,23 +5,20 @@
 
       <thead>
         <tr>
-          <th>ID</th>
           <th>Nimi</th>
           <th>Tyyppi</th>
           <th>Maa</th>
           <th>Hinta (€)</th>
           <th>Määrä (l)</th>
-          <th>Kuvaus</th>
-          <th>Ruokasuositukset</th>
-          <th>URL</th>
         </tr>
       </thead>
 
       <tbody>
-        <!-- TODO: how to skip ID? -->
+        <!-- TODO: wine info should be shown by clicking on a line in the wine table? -->
         <tr v-for="wine in wineStore.wines" :key="wine.id">
-          <td v-for="(value, index) in wine" :key="index">{{value}}</td>
-          <!-- TODO: wine info should be shown by clicking on a line in the wine table? -->
+          <td v-for="attribute in displayedWineAttributes" :key="attribute">
+            {{ wine[attribute] }}
+          </td>
           <button @click="openWineInfo(wine.id)">Info</button>
         </tr>
       </tbody>
@@ -34,12 +31,24 @@
   import WineService from "@/services/WineService.js";
   const wineService = new WineService();
 
+  /*
+  TODO:
+  - Add sorting buttons to table headings: sort by name, type, country, price or quantity.
+  - Reformat rows. Don't show ID, description, food pairings and URL.
+  - Show wine type with the background color of the row?
+  */
+
   export default {
     data() {
       return {
         editing: null,
-        wineStore: wineService.getWineStore()
+        wineStore: wineService.getWineStore(),
+        displayedWineAttributes: ["name", "type", "country", "price", "quantity"]
       }
+    },
+
+    mounted() {
+      wineService.getWines();
     },
 
     methods: {
