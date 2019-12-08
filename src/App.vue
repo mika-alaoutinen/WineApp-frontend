4acc514b7a308891d4c13140ebb9e5f3205f52a6
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-12-08 17:54:58 +0200

    Initiated Vuetify.

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,40 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+  <div id="app">
+    <TheHeader/>
+    <TheNavigation/>
+    <TheFooter/>
+    <router-view/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+  import TheFooter from "@/components/common/TheFooter.vue";
+  import TheHeader from "@/components/common/TheHeader.vue";
+  import TheNavigation from "@/components/common/TheNavigation.vue";
+  export default {
+    name: "app",
+    components: {
+      TheFooter,
+      TheHeader,
+      TheNavigation,
+    },
+  }
 </script>
+
+<style>
+  html, body {
+    height: 100%;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+</style>
