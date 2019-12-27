952db4e2d946b8efee120ceced239c44cd31ac10
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-12-27 17:32:06 +0200

    Added month picker component.

---
+++ src/components/vuetify/MonthPicker.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-menu
+    :close-on-content-click="false"
+    :return-value.sync="date"
+    max-width="290px"
+    min-width="290px"
+    offset-y
+    transition="scale-transition"
+    v-model="displayMenu">
+
+    <template v-slot:activator="{ on }">
+      <v-text-field
+        :label="this.$props.label"
+        readonly
+        v-on="on"
+        v-model="date">
+      </v-text-field>
+    </template>
+
+    <v-date-picker
+      no-title
+      scrollable
+      type="month"
+      v-model="date">
+      <v-spacer />
+      <v-btn @click="displayMenu = false">Peruuta</v-btn>
+      <v-btn @click="saveDate()">OK</v-btn>
+    </v-date-picker>
+  </v-menu>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      date: new Date().toISOString().substr(0, 7),
+      displayMenu: false,
+    }),
+
+    methods: {
+      saveDate() {
+        console.log(this.date);
+        this.$emit("get:date", this.date);
+      }
+    },
+
+    props: {
+      label: { type: String, required: true }
+    }
+  };
+</script>
