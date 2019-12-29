c166fbdfbd822b29eda58674a20b43bf484ee110
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-12-29 20:43:18 +0200

    Added method for saving wines to wine store.

--- src/services/WineService.js
+++ src/services/WineService.js
@@ -7,38 +7,6 @@ class WineService {
         this.wineStore = WineStore;
     }
 
-    /**
-     * @returns WineStore data, i.e. an array of wines.
-     */
-    getWineStore() {
-        return this.wineStore.data;
-    }
-
-    getFromWineStore(id) {
-        return Array
-            .from(this.wineStore.data.wines)
-            .find(wine => wine.id == id);
-    }
-
-    async getWineCount() {
-        return axios
-            .get(UrlBuilder.wine.paths.count)
-            .then(response => response.data)
-            .catch(error => console.error(error));
-    }
-
-    /**
-     * Does a search on backend for wines.
-     * @param {Object} searchParams describing the types of wines to search for.
-     * @returns {Array} found wines.
-     */
-    async search(searchParams) {
-        return axios
-            .get(UrlBuilder.wine.getSearchUrl(searchParams))
-            .then(response => response.data)
-            .catch(error => console.error(error));
-    }
-
 // CRUD operations:
     /**
      * Add all wines received from the backend to wineStore.
@@ -79,6 +47,55 @@ class WineService {
         axios.delete(UrlBuilder.wine.paths.base + id)
              .then(() => this.wineStore.deleteWine(id))
              .catch(error => console.error(error));
+    }
+
+// Other operations:
+    async getWineCount() {
+        return axios
+            .get(UrlBuilder.wine.paths.count)
+            .then(response => response.data)
+            .catch(error => console.error(error));
+    }
+
+    /**
+     * Does a search on backend for wines.
+     * @param {Object} searchParams describing the types of wines to search for.
+     * @returns {Array} found wines.
+     */
+    async search(searchParams) {
+        return axios
+            .get(UrlBuilder.wine.getSearchUrl(searchParams))
+            .then(response => response.data)
+            .catch(error => console.error(error));
+    }
+
+// Winestore related functions:
+    /**
+     * @returns WineStore data, i.e. an array of wines.
+     */
+    getWineStore() {
+        return this.wineStore.data;
+    }
+
+    getFromWineStore(id) {
+        return Array
+            .from(this.wineStore.data.wines)
+            .find(wine => wine.id == id);
+    }
+
+    /**
+     * Saves wines into wine store.
+     * @param {String} storeName should be "wines" or "searchedWines".
+     * @param {Array} wines to save to the store.
+     */
+    saveToWineStore(storeName, wines) {
+        if (storeName === "wines") {
+            this.wineStore.addAll(wines)
+        } else if (storeName === "searchedWines") {
+            this.wineStore.addAllSearchedWines(wines)
+        } else {
+            console.error("Could not find data store '" + storeName + "'.");
+        }
     }
 }
 
