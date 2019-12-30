e2b147bf538849c53e9231118f1e50657b391090
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-12-30 18:13:26 +0200

    Fine tuning style.

--- src/components/wine/WineTable.vue
+++ src/components/wine/WineTable.vue
@@ -4,10 +4,10 @@
     :headers="translateHeaders"
     :items="$props.wines"
     :items-per-page="getItemsPerPage"
+    :search="$props.search"
     loading-text="Haetaan viinejä..."
     no-data-text="Haulla ei löytynyt yhtään tulosta."
-    no-results-text="Haulla ei löytynyt yhtään tulosta."
-    :search="$props.search">
+    no-results-text="Haulla ei löytynyt yhtään tulosta.">
 
     <template v-slot:item.type="{ item }">
       {{ dictionary.translate('wine', item.type) }}
@@ -17,40 +17,40 @@
 </template>
 
 <script>
-import Dictionary from "@/utilities/Dictionary.js";
+  import Dictionary from "@/utilities/Dictionary.js";
 
-export default {
-  computed: {
-    getItemsPerPage() {
-      return this.$props.itemsPerPage === undefined ? 10 : this.$props.itemsPerPage;
+  export default {
+    computed: {
+      getItemsPerPage() {
+        return this.$props.itemsPerPage === undefined ? 10 : this.$props.itemsPerPage;
+      },
+
+      translateHeaders() {
+        const headers = ["name", "type", "country", "price", "volume"];
+        return headers.map(header => ({ 
+          text: this.dictionary.translate("wine", header),
+          value: header
+        }));
+      }
     },
 
-    translateHeaders() {
-      const headers = ["name", "type", "country", "price", "volume"];
-      return headers.map(header => ({ 
-        text: this.dictionary.translate("wine", header),
-        value: header
-      }));
+    data() {
+      return {
+        dictionary: Dictionary,
+      };
+    },
+
+    methods: {
+      openWineInfo(wine) {
+        this.$router.push({ name: "Wine", params: { wineId: "" + wine.id } });
+      }
+    },
+
+    props: {
+      itemsPerPage: { type: Number, required: false },
+      search: { type: String, required: false },
+      wines: { type: Array, required: true }
     }
-  },
-
-  data() {
-    return {
-      dictionary: Dictionary,
-    };
-  },
-
-  methods: {
-    openWineInfo(wine) {
-      this.$router.push({ name: "Wine", params: { wineId: "" + wine.id } });
-    }
-  },
-
-  props: {
-    itemsPerPage: { type: Number, required: false },
-    search: { type: String, required: false },
-    wines: { type: Array, required: true }
-  }
 
 };
 </script>
