a73d5fb0c689768c971be1e33e4c8b83bd5bca51
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-12-30 14:18:49 +0200

    WIP review search.

---
+++ src/components/review/ReviewSearchResults.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card class="full-page-card" max-width="60%" v-show="wineStore.searchDone">
+
+  </v-card>
+</template>
+
+<script>
+  import Dictionary from "@/utilities/Dictionary.js";
+  import ReviewService from "@/services/ReviewService.js";
+
+  const reviewService = new ReviewService();
+
+  export default {
+    data() {
+      return {
+        dictionary: Dictionary,
+        reviewStore: reviewService,
+      }
+    }
+  };
+</script>
--- src/stores/ReviewStore.js
+++ src/stores/ReviewStore.js
@@ -1,11 +1,21 @@
 const ReviewStore = {
     data: {
-        reviews: []
+        reviews: [],
+
+        searched: {
+            searchDone: false,
+            reviews: [],
+        }
     },
 
     addAll(reviewArray) {
         this.data.reviews = reviewArray;
-    }
+    },
+
+    addAllSearchedReviews(reviewArray) {
+        this.data.searched.reviews = reviewArray;
+        this.data.searched.searchDone = true;
+    },
 };
 
 export default ReviewStore;
