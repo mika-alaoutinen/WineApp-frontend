db92495c5eb855f1af8b90507005d46208736796
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-12-31 17:22:29 +0200

    Deleted CRUD methods from wine service.

--- src/services/WineService.js
+++ src/services/WineService.js
@@ -8,69 +8,6 @@ class WineService extends Service {
         super(WineStore);
     }
 
-// CRUD operations:
-    /**
-     * Retrieves a single wine from the back-end based on ID.
-     * @param {Number} id to retrieve from back-end.
-     * @returns {Object} wine.
-     */
-    async getWine(id) {
-        return axios
-            .get(UrlBuilder.wine.paths.base + id)
-            .then(response => response.data)
-            .catch(error => console.error(error));
-    }
-
-    /**
-     * Add all wines received from the backend to store.
-     */
-    async getWines() {
-        return axios
-            .get(UrlBuilder.wine.paths.base)
-            .then(response => super.getStore().addAll(response.data))
-            .catch(error => console.error(error));
-    }
-
-    /**
-     * Sends a new wine to backend for persisting.
-     * @param {Wine} wine 
-     */
-    async postWine(wine) {
-        return axios
-            .post(UrlBuilder.wine.paths.base, wine)
-            .then(response => super.getStore().addWine(response.data))
-            .catch(error => console.error(error));
-    }
-
-    /**
-     * Send an edited wine to backend for persisting.
-     * @param {Number} id 
-     * @param {Wine} editedWine 
-     */
-    async putWine(id, editedWine) {
-        axios.put(UrlBuilder.wine.paths.base + id, editedWine)
-             .then(response => super.getStore().editWine(id, response.data))
-             .catch(error => console.error(error));
-    }
-
-    /**
-     * Sends a delete request to backend.
-     * @param {Number} id 
-     */
-    async deleteWine(id) {
-        axios.delete(UrlBuilder.wine.paths.base + id)
-             .then(() => super.getStore().deleteWine(id))
-             .catch(error => console.error(error));
-    }
-
-// Other operations:
-    async getWineCount() {
-        return axios
-            .get(UrlBuilder.wine.paths.count)
-            .then(response => response.data)
-            .catch(error => console.error(error));
-    }
-
     /**
      * Does a search on backend for wines.
      * @param {Object} searchParams describing the types of wines to search for.
