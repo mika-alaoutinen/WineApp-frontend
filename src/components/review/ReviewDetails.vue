c4e8c477cc59cac5cd925966112db1ef55d87cf9
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-01-02 14:19:39 +0200

    Changed class styles.

--- src/components/review/ReviewDetails.vue
+++ src/components/review/ReviewDetails.vue
@@ -4,36 +4,39 @@
       height="25em"
       src="https://cdn.pixabay.com/photo/2016/10/22/20/34/wine-1761613_960_720.jpg">
     </v-img>
-    <v-card-title class="card-title">Viinin tiedot</v-card-title>
+    <v-card-title class="card-title">Arvostelun tiedot</v-card-title>
 
     <v-row v-for="(value, attribute) in displayReview" :key="attribute">
 
       <!-- Left column for attribute names. -->
-      <v-col align="start" class="column" sm="3">
+      <v-col align="start" sm="3">
         {{ dictionary.translate("review", attribute) }}
       </v-col>
 
       <!-- Right column for values. -->
       <!-- Editing mode: -->
-      <v-col v-if="editing" class="column">
+      <v-col v-if="editing">
         <v-textarea v-if="attribute === 'reviewText'"
           auto-grow
+          class="ma-0 pa-0"
           v-model="review[attribute]">
         </v-textarea>
 
         <v-text-field v-else-if="attribute === 'wine'"
           :value="value.name"
+          class="ma-0 pa-0"
           disabled>
         </v-text-field>
         
         <v-text-field v-else
           @keyup.enter="saveEditedReview(review)"
+          class="ma-0 pa-0"
           v-model="review[attribute]">
         </v-text-field>
       </v-col>
 
       <!-- View mode: -->
-      <v-col v-else align="start" class="column">
+      <v-col v-else align="start">
         <div v-if="attribute === 'wine'">{{ value.name }}</div>
         <div v-else>{{ value }}</div>
       </v-col>
@@ -110,12 +113,8 @@
 
 <style scoped>
   .card-title { padding-left: 0 }
-  .column {
+  .col {
     padding-bottom: 6px;
     padding-top: 6px;
-  }
-  .v-text-field {
-    margin-top: 0;
-    padding-top: 0;
   }
 </style>
