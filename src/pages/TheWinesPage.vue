94b6af769393fbc9c5e8531b752cb951b760eadd
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-01-04 16:34:04 +0200

    Refactored reviews page to be consistent with wine reviews page.

--- src/pages/TheWinesPage.vue
+++ src/pages/TheWinesPage.vue
@@ -21,13 +21,9 @@
 <script>
   import WineTable from "@/components/wine/WineTable.vue";
 
-  /*
-  TODO:
+  /* TODO:
     - Bug fix: when wine is deleted in WineDetails, it still shows up in wine table.
       Deleted wine disappears once a different page is loaded.
-    - Show wine type with the background color of the row?
-    - Implement filters: only show red wines, wines with volume = 0.75, etc.
-    - Use v-chip to present active filters?
   */
 
   export default {
