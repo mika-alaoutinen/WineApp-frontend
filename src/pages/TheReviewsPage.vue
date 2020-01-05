f437ecc00e9784a37908390e77e664e33bd79475
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-01-05 12:57:08 +0200

    Refactored review search, now works correctly.

--- src/pages/TheReviewsPage.vue
+++ src/pages/TheReviewsPage.vue
@@ -2,17 +2,26 @@
   <v-card class="full-page-card" max-width="75%">
     <v-card-title class="card-title">Uusimmat arvostelut</v-card-title>
 
-    <ReviewTable/>
+    <ReviewTable :reviews="reviewStore.reviews"/>
 
   </v-card>
 </template>
 
 <script>
   // TODO: Add functionality to load more reviews. => I.e. page two has reviews 11-20, etc.
+  // TODO: delete this page?
 
+  import ReviewService from "@/services/ReviewService.js";
   import ReviewTable from "@/components/review/ReviewTable.vue";
 
   export default {
     components: { ReviewTable },
+
+    data() {
+      return {
+        reviewStore: new ReviewService().getStore().data,
+      }
+    },
+
   };
 </script>
