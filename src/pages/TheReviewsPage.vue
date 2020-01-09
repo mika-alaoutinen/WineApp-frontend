ba3beecba46efb411eba2131323473f719652bbd
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-01-09 19:33:52 +0200

    Refactored reviews page to be similar to wines page.

--- src/pages/TheReviewsPage.vue
+++ src/pages/TheReviewsPage.vue
@@ -1,8 +1,19 @@
 <template>
   <v-card class="full-page-card" max-width="75%">
-    <v-card-title class="card-title">Arvostelut</v-card-title>
+    <v-card-title class="card-title">Arvostelut<v-spacer/>
+      <v-text-field
+        class="search-bar"
+        hide-details
+        label="Hae arvosteluja"
+        single-line
+        v-model="search">
+      </v-text-field>   
+    </v-card-title>
 
-    <ReviewTable :reviews="reviewStore.reviews"/>
+    <ReviewTable
+      :itemsPerPage="15"
+      :reviews="reviewStore.reviews"
+      :search="search"/>
   </v-card>
 </template>
 
@@ -16,6 +27,7 @@
     data() {
       return {
         reviewStore: new ReviewService().getStore().data,
+        search: "",
       }
     },
 
--- src/pages/TheWinesPage.vue
+++ src/pages/TheWinesPage.vue
@@ -11,7 +11,7 @@
     </v-card-title>
 
     <WineTable
-      :itemsPerPage=15
+      :itemsPerPage="15"
       :search="search"
       :wines="wineStore.wines">
     </WineTable>
