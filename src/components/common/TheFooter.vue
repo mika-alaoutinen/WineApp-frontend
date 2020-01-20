c2bdb96b434d05a314d3b1fdc5c85f9d9d45de67
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-01-20 20:29:13 +0200

    Add icons to footer.

--- src/components/common/TheFooter.vue
+++ src/components/common/TheFooter.vue
@@ -1,13 +1,35 @@
 <template>
   <v-footer absolute class="primary" padless>
     <v-spacer class="footer">
-      Lähdekoodi <a class="url" href="https://github.com/mika-alaoutinen/WineApp-frontend">GitHubissa</a>
+      <v-icon large>{{ gitHubIcon }}</v-icon>
+      Lähdekoodi:
+
+      <v-icon large>{{ vueIcon }}</v-icon>
+      <a href="https://github.com/mika-alaoutinen/WineApp-frontend">frontend</a>
+
+      <v-icon large>{{ javaIcon }}</v-icon>
+      <a href="https://github.com/mika-alaoutinen/WineApp">backend</a>
     </v-spacer>
   </v-footer>
 </template>
 
+<script>
+  import { mdiGithubCircle, mdiLanguageJava, mdiVuejs } from '@mdi/js';
+
+  export default {
+    data() {
+      return {
+        gitHubIcon: mdiGithubCircle,
+        javaIcon: mdiLanguageJava,
+        vueIcon: mdiVuejs,
+      }
+    },
+  }
+</script>
+
 <style scoped>
-  .footer { padding: 1em }
-  .footer, .url { color: whitesmoke !important; }
+  a, .footer, .v-icon { color: whitesmoke !important }
+  .footer { padding: 0.5em }
   .v-footer { opacity: 0.9 }
+  .v-icon { margin-left: 0.5em }
 </style>
