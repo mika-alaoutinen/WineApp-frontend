c0e3543c0d0c8a6ef69822b6bd15a2a922fb358f
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-01-20 21:47:44 +0200

    Added functionality to retrieve newest review on home page.

--- src/pages/TheHomePage.vue
+++ src/pages/TheHomePage.vue
@@ -3,12 +3,28 @@
     <v-card-title class="card-title secondary--text">Aloitus</v-card-title>
     <p>{{ wineCount }} maisteltua viiniä.</p>
     <p>{{ reviewCount }} Kirjoitettua arvostelua.</p>
+
+    <v-divider/>
+
+    <p>Uusin arvostelu:</p>
+    <p>
+      <v-icon>{{ wineIcon }}</v-icon>
+      {{ wine.name }}
+    </p>
+    <p>
+      <v-icon>{{ reviewerIcon }}</v-icon>
+      {{ newestReview.author }}
+
+      <v-icon class="ratingIcon">{{ ratingIcon }}</v-icon>
+      {{ newestReview.rating }}
+    </p>
   </v-card>
 </template>
 
 <script>
   import WineService from "@/services/WineService.js";
   import ReviewService from "@/services/ReviewService.js";
+  import { mdiAccount, mdiBottleWine, mdiStar } from '@mdi/js';
 
   const wineService = new WineService();
   const reviewService = new ReviewService();
@@ -16,17 +32,31 @@
   export default {
     data() {
       return {
+        ratingIcon: mdiStar,
+        reviewerIcon: mdiAccount,
+        wineIcon: mdiBottleWine,
+
+        newestReview: {},
+        wine: {},
+
+        reviewCount: 0,
         wineCount: 0,
-        reviewCount: 0
       }
     },
 
     mounted() {
       wineService.getCount().then(wines => this.wineCount = wines);
       reviewService.getCount().then(reviews => this.reviewCount = reviews);
+
+      // Get the newest review:
+      reviewService.quickSearch("newest", 1)
+                   .then(reviewArray => this.newestReview = reviewArray[0])
+                   .then(newestReview => this.wine = newestReview.wine);
     }
   };
 </script>
 <style scoped>
   p { text-align: left }
+  .ratingIcon { margin-left: 1em }
+  .v-divider { margin-bottom: 1em }
 </style>
