0e7a46e4bd4a4a0f44b06030813ff8047768e2cc
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-02-02 19:48:00 +0200

    Add router link stub.

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
-import { createLocalVue, mount } from '@vue/test-utils';
+import { createLocalVue, mount, RouterLinkStub } from '@vue/test-utils';
 
 Vue.config.productionTip = false;
 Vue.use(Vuetify);
@@ -27,6 +27,7 @@ export function mountVuetifyComponent(Component, propsData) {
         localVue,
         vuetify,
         propsData,
+        stubs: { RouterLink: RouterLinkStub },
     });
 }
 
