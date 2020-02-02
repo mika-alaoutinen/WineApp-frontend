dce76dfc089beaaa489f55552e83d4c1845bc699
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-02-02 20:27:37 +0200

    WIP ReviewTable test.

--- tests/unit/ReviewTable.spec.js
+++ tests/unit/ReviewTable.spec.js
@@ -2,16 +2,28 @@ import ReviewTable from "@/components/review/ReviewTable.vue";
 import { mountVuetifyComponent } from "../index.js";
 import { reviews } from "../testdata.js";
 
-test("component contains a data table", () => {
-    const wrapper = mountComponent(ReviewTable);
+const wrapper = mountComponent();
+
+test("reviews prop is received correctly", () => {
     expect(wrapper.props("reviews")).toHaveLength(2);
 });
 
+// Computed properties:
+test("itemsPerPage prop is given default value when it's undefined", () => {
+    expect(wrapper.vm.getItemsPerPage).toBe(10);
+});
+
+test("itemsPerPage prop is received correctly", () => {
+    wrapper.setProps({ itemsPerPage: 20 });
+    expect(wrapper.props("itemsPerPage")).toBe(20);
+    expect(wrapper.vm.getItemsPerPage).toBe(20);
+});
+
 // Utility functions:
-function mountComponent() {
+function mountComponent(itemsPerPage) {
     const propsData = {
         reviews: reviews,
-        itemsPerPage: 10,
+        itemsPerPage: itemsPerPage,
         search: "",
     }
 
