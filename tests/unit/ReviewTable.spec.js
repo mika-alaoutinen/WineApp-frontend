55f6e8a0629e24b1c22d2aab6f5bcb35e1264411
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-02-05 19:00:00 +0200

    Some more review table tests.

--- tests/unit/ReviewTable.spec.js
+++ tests/unit/ReviewTable.spec.js
@@ -4,12 +4,18 @@ import { reviews } from "../testdata.js";
 
 const wrapper = mountComponent();
 
+// Sanity check props:
 test("reviews prop is received correctly", () => {
     expect(wrapper.props("reviews")).toHaveLength(2);
 });
 
+test("search prop is received correctly", () => {
+    wrapper.setProps({ search: "haku" });
+    expect(wrapper.props("search")).toEqual("haku");
+});
+
 // Computed properties:
-test("itemsPerPage prop is given default value when it's undefined", () => {
+test("itemsPerPage prop is given default value of 10 when it's undefined", () => {
     expect(wrapper.vm.getItemsPerPage).toBe(10);
 });
 
@@ -17,6 +23,12 @@ test("itemsPerPage prop is received correctly", () => {
     wrapper.setProps({ itemsPerPage: 20 });
     expect(wrapper.props("itemsPerPage")).toBe(20);
     expect(wrapper.vm.getItemsPerPage).toBe(20);
+});
+
+// Methods
+test("The first sentence is retrieved from a review text paragraph, ending with '...' ", () => {
+    const reviewText = reviews[0].reviewText;
+    expect(wrapper.vm.getExcerpt(reviewText)).toEqual("Viinin 1 arvostelu...");
 });
 
 // Utility functions:
