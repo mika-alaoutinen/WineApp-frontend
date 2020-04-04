d7875c8b0e63ce51e6b2f879f1d009c360e20926
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-04-04 15:49:09 +0300

    Fix DatePicker tests

--- tests/unit/DatePicker.spec.js
+++ tests/unit/DatePicker.spec.js
@@ -1,24 +1,37 @@
 import DatePicker from '@/components/vuetify/DatePicker.vue'
 import { mountVuetifyComponent } from '../index.js'
 
-// Test that component loads correctly with default values:
-test('component loads correctly with default values', () => {
-  const wrapper = defaultMount()
-  expect(wrapper.vm.type).toBe('date')
-  expect(wrapper.vm.date).toBe(formatDate())
+const today = new Date().toISOString()
+const todayCompare = today.substr(0, 15) // today's date at a reasonable accuracy
+
+describe('Date calendar', () => {
+  test('Default calendar format is date', () => {
+    const wrapper = defaultMount()
+    expect(wrapper.vm.$props.calendarType).toBe('date')
+    expect(wrapper.vm.date).toContain(todayCompare)
+    expect(wrapper.vm.dateString).toBe(formatDate(today))
+  })
+
+  test('date is given as prop', () => {
+    const date = new Date(2019, 11, 17).toISOString()
+    const wrapper = mountComponent('date', true, 'Päivämäärä', date)
+    expect(wrapper.vm.$props.calendarType).toBe('date')
+    expect(wrapper.vm.dateString).toBe(formatDate(date))
+  })
 })
 
-test('component loads correctly with month calendar', () => {
-  const wrapper = mountComponent('month', true, 'Kuukausi')
-  expect(wrapper.vm.type).toBe('month')
-  expect(wrapper.vm.date).toBe(formatMonth())
-})
+describe('Month calendar', () => {
+  test('default date is current month', () => {
+    const wrapper = mountComponent('month', true, 'Kuukausi')
+    expect(wrapper.vm.$props.calendarType).toBe('month')
+    expect(wrapper.vm.dateString).toBe(formatMonth(today))
+  })
 
-test('component loads correctly if a selected date is given as prop', () => {
-  const date = formatDate(new Date(2019, 11, 17))
-  const wrapper = mountComponent('date', true, 'Päivämäärä', date)
-  expect(wrapper.vm.type).toBe('date')
-  expect(wrapper.vm.date).toBe(date)
+  test('month is given as prop', () => {
+    const wrapper = mountComponent('month', true, 'Kuukausi', today)
+    expect(wrapper.vm.$props.calendarType).toBe('month')
+    expect(wrapper.vm.dateString).toBe(formatMonth(today))
+  })
 })
 
 // Utility functions:
@@ -40,12 +53,5 @@ function mountComponent(calendarType, isEnabled, labelText, selectedDate) {
   return mountVuetifyComponent(DatePicker, propsData)
 }
 
-function formatDate(date) {
-  return date === undefined
-    ? new Date().toISOString().split('T')[0]
-    : date.toISOString().split('T')[0]
-}
-
-function formatMonth() {
-  return new Date().toISOString().substr(0, 7)
-}
+const formatDate = date => date.split('T')[0]
+const formatMonth = month => month.substr(0, 7)
--- tests/unit/ReviewTable.spec.js
+++ tests/unit/ReviewTable.spec.js
@@ -4,31 +4,34 @@ import { reviews } from '../testdata.js'
 
 const wrapper = mountComponent()
 
-// Sanity check props:
-test('reviews prop is received correctly', () => {
-  expect(wrapper.props('reviews')).toHaveLength(2)
+describe('Sanity checks', () => {
+  test('reviews prop is received correctly', () => {
+    expect(wrapper.props('reviews')).toHaveLength(2)
+  })
+
+  test('search prop is received correctly', () => {
+    wrapper.setProps({ search: 'haku' })
+    expect(wrapper.props('search')).toEqual('haku')
+  })
 })
 
-test('search prop is received correctly', () => {
-  wrapper.setProps({ search: 'haku' })
-  expect(wrapper.props('search')).toEqual('haku')
+describe('Computed properties', () => {
+  test('itemsPerPage prop is given default value of 10 when it\'s undefined', () => {
+    expect(wrapper.vm.getItemsPerPage).toBe(10)
+  })
+
+  test('itemsPerPage prop is received correctly', () => {
+    wrapper.setProps({ itemsPerPage: 20 })
+    expect(wrapper.props('itemsPerPage')).toBe(20)
+    expect(wrapper.vm.getItemsPerPage).toBe(20)
+  })
 })
 
-// Computed properties:
-test('itemsPerPage prop is given default value of 10 when it\'s undefined', () => {
-  expect(wrapper.vm.getItemsPerPage).toBe(10)
-})
-
-test('itemsPerPage prop is received correctly', () => {
-  wrapper.setProps({ itemsPerPage: 20 })
-  expect(wrapper.props('itemsPerPage')).toBe(20)
-  expect(wrapper.vm.getItemsPerPage).toBe(20)
-})
-
-// Methods
-test('The first sentence is retrieved from a review text paragraph, ending with \'...\' ', () => {
-  const { reviewText } = reviews[0]
-  expect(wrapper.vm.getExcerpt(reviewText)).toEqual('Viinin 1 arvostelu...')
+describe('Methods', () => {
+  test('The first sentence is retrieved from a review text paragraph, ending with \'...\' ', () => {
+    const { reviewText } = reviews[0]
+    expect(wrapper.vm.getExcerpt(reviewText)).toEqual('Viinin 1 arvostelu...')
+  })
 })
 
 // Utility functions:
