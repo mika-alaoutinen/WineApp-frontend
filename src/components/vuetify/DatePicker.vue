474242147c44f44fad35c1e5ab833bda98d0984c
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-04-04 14:16:18 +0300

    Refactor DatePicker

--- src/components/vuetify/DatePicker.vue
+++ src/components/vuetify/DatePicker.vue
@@ -7,7 +7,7 @@
   >
     <template v-slot:activator="{ on }">
       <v-text-field
-        v-model="date"
+        v-model="dateString"
         :label="$props.labelText"
         readonly
         v-on="on"
@@ -26,16 +26,24 @@
 
 <script>
   export default {
-
     props: {
       enabled: { type: Boolean, required: true },
       labelText: { type: String, required: true },
       calendarType: { type: String, required: false, default: 'date' },
-      selectedDate: { type: String, required: false },
+      selectedDate: { type: String, required: false, default: new Date().toISOString() },
     },
+
     data() {
       return {
-        date: new Date(),
+        date: this.$props.selectedDate,
+      }
+    },
+
+    computed: {
+      dateString() {
+        return this.$props.calendarType === 'date'
+          ? this.date.split('T')[0]
+          : this.date.substr(0, 7)
       }
     },
 
@@ -45,18 +53,5 @@
       }
     },
 
-    mounted() {
-      // If date is given in props, use it as is:
-      if (this.$props.selectedDate !== undefined) {
-        this.date = this.$props.selectedDate
-        return
-      }
-
-      // Format date string based on calendar type: 1) regular calendar 2) month calendar
-      this.date = this.$props.calendarType === 'date'
-        ? this.date.toISOString().split('T')[0]
-        : this.date.toISOString().substr(0, 7)
-
-    },
   }
 </script>
