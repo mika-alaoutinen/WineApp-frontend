86c7de62e3452fb3b4095a97cc0c9b3135ab264a
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-04-05 11:11:49 +0300

    Fix WineTable tests

--- tests/unit/WineTable.spec.js
+++ tests/unit/WineTable.spec.js
@@ -4,25 +4,30 @@ import { wines } from '../testdata.js'
 
 const wrapper = mountComponent()
 
-// Sanity check props:
-test('wines prop is received correctly', () => {
-  expect(wrapper.props('wines')).toHaveLength(2)
-})
+describe('Props values', () => {
+  test('itemsPerPage default value is 10', () => {
+    expect(wrapper.props('itemsPerPage')).toBe(10)
+  })
 
-test('search prop is received correctly', () => {
-  wrapper.setProps({ search: 'haku' })
-  expect(wrapper.props('search')).toEqual('haku')
-})
+  test('itemsPerPage is received correctly', () => {
+    wrapper.setProps({ itemsPerPage: 20 })
+    expect(wrapper.props('itemsPerPage')).toBe(20)
+  })
 
-// Computed properties:
-test('itemsPerPage prop is given default value of 10 when it\'s undefined', () => {
-  expect(wrapper.vm.getItemsPerPage).toBe(10)
-})
+  test('search prop is empty by default', () => {
+    expect(wrapper.props('search')).toEqual('')
+  })
 
-test('itemsPerPage prop is received correctly', () => {
-  wrapper.setProps({ itemsPerPage: 20 })
-  expect(wrapper.props('itemsPerPage')).toBe(20)
-  expect(wrapper.vm.getItemsPerPage).toBe(20)
+  test('search prop is received correctly', () => {
+    wrapper.setProps({ search: 'haku' })
+    expect(wrapper.props('search')).toEqual('haku')
+  })
+
+  test('wines prop is received correctly', () => {
+    const wine1 = wrapper.props('wines')[0]
+    expect(wrapper.props('wines')).toHaveLength(2)
+    expect(wine1.name).toBe('Viini 1')
+  })
 })
 
 // Utility functions:
@@ -32,6 +37,5 @@ function mountComponent(itemsPerPage) {
     search: '',
     wines,
   }
-
   return mountVuetifyComponent(WineTable, propsData)
 }
