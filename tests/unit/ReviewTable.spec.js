1b8cee0699d675497be25071c4a9128b44db8398
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-04-05 11:20:22 +0300

    Fixed all broken tests

--- tests/unit/ReviewTable.spec.js
+++ tests/unit/ReviewTable.spec.js
@@ -4,9 +4,24 @@ import { reviews } from '../testdata.js'
 
 const wrapper = mountComponent()
 
-describe('Sanity checks', () => {
+describe('Props values', () => {
+  test('itemsPerPage default value is 10', () => {
+    expect(wrapper.props('itemsPerPage')).toBe(10)
+  })
+
+  test('itemsPerPage is received correctly', () => {
+    wrapper.setProps({ itemsPerPage: 20 })
+    expect(wrapper.props('itemsPerPage')).toBe(20)
+  })
+
   test('reviews prop is received correctly', () => {
+    const review1 = wrapper.props('reviews')[0]
     expect(wrapper.props('reviews')).toHaveLength(2)
+    expect(review1.author).toBe('Mika')
+  })
+
+  test('search prop is empty by default', () => {
+    expect(wrapper.props('search')).toEqual('')
   })
 
   test('search prop is received correctly', () => {
@@ -15,20 +30,8 @@ describe('Sanity checks', () => {
   })
 })
 
-describe('Computed properties', () => {
-  test('itemsPerPage prop is given default value of 10 when it\'s undefined', () => {
-    expect(wrapper.vm.getItemsPerPage).toBe(10)
-  })
-
-  test('itemsPerPage prop is received correctly', () => {
-    wrapper.setProps({ itemsPerPage: 20 })
-    expect(wrapper.props('itemsPerPage')).toBe(20)
-    expect(wrapper.vm.getItemsPerPage).toBe(20)
-  })
-})
-
 describe('Methods', () => {
-  test('The first sentence is retrieved from a review text paragraph, ending with \'...\' ', () => {
+  test('The first sentence is retrieved from a review text paragraph, ending with "..."', () => {
     const { reviewText } = reviews[0]
     expect(wrapper.vm.getExcerpt(reviewText)).toEqual('Viinin 1 arvostelu...')
   })
@@ -41,6 +44,5 @@ function mountComponent(itemsPerPage) {
     itemsPerPage,
     search: '',
   }
-
   return mountVuetifyComponent(ReviewTable, propsData)
 }
