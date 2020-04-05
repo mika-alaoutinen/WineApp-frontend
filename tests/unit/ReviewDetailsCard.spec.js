ee0702c7b0334e1349fa290a02004a32276a87d3
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-04-05 14:02:27 +0300

    WIP ReviewDetailsCard tests

--- tests/unit/ReviewDetailsCard.spec.js
+++ tests/unit/ReviewDetailsCard.spec.js
@@ -6,7 +6,9 @@ const wrapper = mountComponent()
 
 // Sanity check props:
 test('reviews prop is received correctly', () => {
+  const review1 = wrapper.props('reviews')[0]
   expect(wrapper.props('reviews')).toHaveLength(2)
+  expect(review1.author).toBe('Mika')
 })
 
 // Utility functions:
