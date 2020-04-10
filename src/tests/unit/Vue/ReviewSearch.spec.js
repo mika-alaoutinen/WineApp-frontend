253d226dd6b86db600c5e778f71c2fec15ba21b1
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-04-10 17:51:08 +0300

    WIP ReviewsSearch tests

---
+++ src/tests/unit/Vue/ReviewSearch.spec.js
@@ -0,0 +1,5 @@
+import ReviewSearch from '@/components/review/ReviewSearch'
+import { mountVuetifyComponent } from '@/tests/index.js'
+import { reviews } from '@/tests/testdata.js'
+
+// Utility functions
