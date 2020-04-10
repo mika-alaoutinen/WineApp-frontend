2755c1339dd54a04b7ad52aaf0ad523e62aec690
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-04-10 11:23:35 +0300

    Add TheNavigation test

---
+++ tests/unit/Vue/TheNavigation.spec.js
@@ -0,0 +1,14 @@
+import { mountVuetifyComponent } from '../../index.js'
+import TheNavigation from '@/components/common/TheNavigation.vue'
+
+const wrapper = mountVuetifyComponent(TheNavigation)
+
+describe('Navigation menu buttons', () => {
+  test('navigation menu has five buttons', () => {
+    const foundButtons = wrapper.findAll('.v-btn')
+    const expectedNavButtons = ['Aloitus', 'Arvostelut', 'Viinit', 'Lisää uusi', 'Haku']
+    expectedNavButtons.forEach(button =>
+      expect(foundButtons.contains(button))
+    )
+  })
+})
