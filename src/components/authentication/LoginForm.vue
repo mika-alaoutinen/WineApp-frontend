2d06222b011a5ff6d0ab002e55c75577e127aced
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-05-09 13:10:29 +0300

    Add forms for login and registration

---
+++ src/components/authentication/LoginForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card
+    class="full-page-card"
+    min-width="30%"
+    max-width="60%"
+  >
+    <v-card-title class="card-title secondary--text">
+      Kirjaudu sisään
+    </v-card-title>
+
+    <p>Käyttäjänimi</p>
+  </v-card>
+</template>
+
+<script>
+  import { login, register } from '@/services/AuthenticationService.js'
+
+  export default {
+    data() {
+      return {
+        user: {
+          username: '',
+          password: '',
+        }
+      }
+    },
+
+    methods: {
+      doLogin() {
+
+      }
+    },
+
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/authentication/RegisterUserForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card class="full-page-card">
+    <v-card-title class="card-title secondary--text">
+      Kirjaudu sisään
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
