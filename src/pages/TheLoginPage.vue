d413091a918b8f5563f0239a97229274c094539c
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-05-09 12:53:36 +0300

    Add TheLoginPage

---
+++ src/pages/TheLoginPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card
+    class="full-page-card"
+    min-width="30%"
+    max-width="60%"
+  >
+    <v-card-title class="card-title secondary--text">
+      Kirjaudu sisään
+    </v-card-title>
+
+    <p>Käyttäjänimi</p>
+  </v-card>
+</template>
+
+<script>
+  import { login, register } from '@/services/AuthenticationService.js'
+
+  export default {
+    data() {
+      return {
+        user: {
+          username: '',
+          password: '',
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
