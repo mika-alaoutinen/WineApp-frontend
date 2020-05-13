d08c00f05ed18c011fb322153cc7ad8407e81788
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-05-13 20:18:32 +0300

    Ready to merge and deploy

--- src/utilities/UrlBuilder.js
+++ src/utilities/UrlBuilder.js
@@ -1,5 +1,5 @@
-const basePath = 'http://localhost:8080/api/'
-// const basePath = 'https://maistissa-backend.herokuapp.com/api/'
+// const basePath = 'http://localhost:8080/api/'
+const basePath = 'https://maistissa-backend.herokuapp.com/api/'
 const authPath = `${basePath}auth/`
 const userPath = `${basePath}users/`
 const reviewPath = `${basePath}reviews/`
