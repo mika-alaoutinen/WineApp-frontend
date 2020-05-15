b923b7b65ab79fbcf4d6195d8f87c194f281b749
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-05-15 19:06:28 +0300

    Add backend URL as environment variable

--- src/utilities/UrlBuilder.js
+++ src/utilities/UrlBuilder.js
@@ -1,5 +1,4 @@
-// const basePath = 'http://localhost:8080/api/'
-const basePath = 'https://maistissa-backend.herokuapp.com/api/'
+const basePath = process.env.BACKEND_URL ? `${process.env.BACKEND_URL}/` : 'http://localhost:8080/api/'
 const authPath = `${basePath}auth/`
 const userPath = `${basePath}users/`
 const reviewPath = `${basePath}reviews/`
